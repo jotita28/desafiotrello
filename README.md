17c0c47996733df9407fb0fc650f83d47f35b30e
Jorge <jorgepaillan@gmail.com>
2019-03-22 15:54:09 -0300

    agilidad2

---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+
+Desafio Agilidad 2
+
+Link a trello:
+
+https://trello.com/invite/b/PVdeHl2u/3016180b87c28b76a206b314a463f33b/desafio-agilidad-2
---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+20.times do
+Product.create(
+    name: Faker::Commerce.product_name,
+    address: 'Las parcelas 123',
+    price: rand(1..50),
+    user_id: 1
+)
+end
